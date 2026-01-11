09fdb30f083a2bbf0e82f990558f6e8efb08c91d
Suhrid Singh <suhrids88@gmail.com>
2026-01-11 13:45:24 +0530

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cin>>a >>b >>c >>d >>e ;
+    
+    cout<<a<<endl;  
+    cout<<b<<endl;  
+    cout<<c<<endl;  
+    cout<<fixed<<setprecision(3)<<d<<endl;  
+    cout<<fixed<<setprecision(9)<<e<<endl;  
+    
+    // scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+    // printf("%d \n %ld \n %c \n %f \n %lf",a,b,c,d,e);
+    return 0;
+}
